50112d8ccc10bd83477b18b1aa4783a77f48a8ef
David Ostler <david.ostler001@gmail.com>
2016-10-30 12:34:51 -0600

    Fixed threaded download issue; HEAD bytes returned compressed total value

--- downloadAccelerator.py
+++ downloadAccelerator.py
@@ -21,9 +21,9 @@ class SharedList:
         self.list.append(item)
         self.sem.release()
 
-    def getFinishedFile(self):
+    def writeToFile(self):
+        sorted(self.list, key=lambda l:l[1])
         print self.list
-        return
 
 ####################
 # Downloader Class #
@@ -36,21 +36,13 @@ class Downloader(threading.Thread):
        self.endByte = endByte
        self.url = url
        self.sharedList = sharedList
-       threading.Thread.__init__(self) 
+       threading.Thread.__init__(self)
 
     def run(self):
-       # print "Hello from thread", self.getName(), self.startByte, self.endByte
-       # print "self.url:", self.url
-       print 'self.startByte:', self.startByte
-       print 'self.endByte:', self.endByte
        rangeBody = 'bytes={self.startByte}-{self.endByte}'.format(**locals())
-       # print 'RangeBody:', rangeBody
-       # response = requests.get(self.url, headers={ 'Range': rangeBody, 'Accept-Encoding': 'identity'})
-       response = requests.get(self.url, headers={ 'Range': rangeBody })
+       response = requests.get(self.url, headers={ 'Range': rangeBody, 'Accept-Encoding': 'identity'})
        if str(response.status_code)[0] == '2':
         self.sharedList.add([response.text, self.startByte])
-       print response
-       print response.text
 
 
 
@@ -87,13 +79,13 @@ url = sys.argv[2]
 # CHECK: url is valid
 try:
     response = requests.head(url, headers={'Accept-Encoding':'identity'})
-    print response.headers['Accept-Ranges']
     contentLength = int(response.headers['Content-Length'])
-    print "Content-Length", contentLength
     if str(response.status_code)[0] == "4" or str(response.status_code)[0] == "5":
 	raise Exception()
 except:
     print "Arg Error: Invalid URL provided."
+    print
+    print usage
     exit()
 
 
@@ -113,7 +105,6 @@ sl = SharedList()
 threads = []
 
 for i in range(0, numThreads):
-    # print "thread" , i+1
     d = Downloader(startByte, endByte, url, sl)
     threads.append(d)
 
@@ -132,15 +123,5 @@ for t in threads:
 for t in threads:
     t.join()
 
-
-sl.getFinishedFile()
-
-
-
-
-
-
-
-
-
-
+#
+sl.writeToFile()
